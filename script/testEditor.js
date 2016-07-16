38f70e1d0577d991f63c6724b225e1fb76e61a23
Виктор <shark_vik@inbox.ru>
2016-07-16 17:09:23 +0300

    рефатокринг

--- script/testEditor.js
+++ script/testEditor.js
@@ -3,17 +3,14 @@
         scope:null,
         editor:null,
         initialized: null,
+        $textEditor: null,
 
         root: function( $scope )
         {
             this.scope = $scope;
-            this.initTestEditor( $.proxy( this.initEditorActions, this) );
+            this.$textEditor = $("#texteditor");
+            this.$textEditor.ckeditor( $.proxy( this.initEditorActions, this) );
             this.initialized = true;
-        },
-
-        initTestEditor: function( callback )
-        {
-            $("#texteditor").ckeditor( callback );
         },
 
         getText: function()
@@ -25,15 +22,15 @@
         },
 
         updateText: function () {
-            controller.scope.text = controller.getText();
+            this.scope.text = this.getText();
         },
 
         initEditorActions: function(){
-            this.editor = $('#texteditor').ckeditorGet();
-            this.scope.update = this.updateText;
-            this.editor.on( 'change', function(){
-                $('#texteditor').keyup();
-            } );
+            this.editor = this.$textEditor.ckeditorGet();
+            this.scope.update = $.proxy( this.updateText, this );
+            this.editor.on( 'change', $.proxy( function(){
+                this.$textEditor.keyup();
+            }, this ) );
         }
     };
     $( document ).on( 'content-loaded', function( ev, $scope ){
---
+++ script/yandexMapCover.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Viktor on 16.07.2016.
+ */
+(function($){
+    $.yandexMap = function( $item, points ){
+        var settings = $item.data();
+        var center = [settings.centerX || 59.964584, settings.centerY || 30.314979];
+        this.zoom = settings.zoom || 15;
+        this.center = center;
+        this.points = points || [];
+        this.$root = $item;
+        this.init();
+    };
+
+    $.yandexMap.prototype = {
+        center: null,
+        zoom: null,
+        points: null,
+        $root: null,
+
+        init: function()
+        {
+            ymaps.ready( $.proxy( this.build, this ) );
+        },
+
+        build: function()
+        {
+            if( !this.$root.attr( 'id' ) )
+            {
+                this.$root.attr( 'id', 'map' + (new Date()) );
+            }
+            var myMap = new ymaps.Map(this.$root.attr('id'), {
+                center: this.center,
+                zoom: this.zoom
+            });
+            var objectManager = new ymaps.ObjectManager({clusterize: true});
+            objectManager.add(this.getData());
+            objectManager.objects.setObjectOptions('preset', 'islands#greenDotIcon');
+            myMap.geoObjects.add(objectManager);
+        },
+
+        getData: function()
+        {
+            var data = {
+                "type": "FeatureCollection",
+                "features": []
+            };
+
+            var count = this.points.length;
+            $.each( this.points, function( index, item ){
+                data.features.push(
+                    {
+                        "type": "Feature",
+                        "id": count + index,
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": item.coordinates
+                        },
+                        "properties": {
+                            "clusterCaption": item.name,
+                            "balloonContent": item.text,
+                            "hintContent": item.title
+                        }
+                    }
+                )
+            } );
+            return data;
+        }
+    };
+
+    $.fn.yandexMap = function(){
+        var $item = $( this );
+        var $pointObjects = $( '*[data-point]', $item );
+        var points = [];
+        $pointObjects.each( function( index, item ){
+            var data = $( item ).data();
+            points.push({
+                coordinates: [data.x, data.y],
+                name: data.name,
+                text: data.text,
+                title: data.title
+            });
+        } );
+        debugger;
+        return new $.yandexMap( $item, points );
+    };
+})(jQuery);
